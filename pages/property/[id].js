3f306ff87c4830cf0df1624a522cfefd3b41d5f8
simonmedici <simon.sebastian.medici@gmail.com>
2022-06-06 22:54:25 -0500

    add searchfilters ImageScrollbar and search js

---
+++ pages/property/[id].js
@@ -0,0 +1,26 @@
+import { Box, Flex, Spacer, Text, Avatar} from '@chakra-ui/react';
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import millify from 'millify';
+import DefaultImage from '../assets/images/house.jpeg';
+
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
+
+const PropertyDetails = ({ price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos }) => (
+    <Box maxWidth='1000px' margin='auto' p='4'>
+        {photos && <ImageScrollbar data}
+    </Box>
+);
+
+export default PropertyDetails;
+
+export async function getServerSideProps({ params: { id }}) {
+    const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+
+    return {
+        props: {
+            propertyDetails: data
+        }
+    }
+}
